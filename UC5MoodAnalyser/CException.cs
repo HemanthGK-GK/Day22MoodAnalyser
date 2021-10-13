8258e925ad62e9e06e7651a6f8211f1f72bddbdb
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-13 17:44:07 +0530

    Hemanth : MoodAnalyser with Parameter constructor

---
+++ UC5MoodAnalyser/CException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC5MoodAnalyser
+{
+    class CException:Exception
+    {
+        public enum ExceptionType
+        {
+            null_message,
+            empty_message,
+            no_such_field,
+            no_such_method,
+            no_such_class,
+            object_creation_issue
+        }
+        private ExceptionType type;
+        public CException(ExceptionType Type, String message) : base(message)
+        {
+            this.type = Type;
+        }
+    }
+}
---
+++ UC5MoodAnalyser/MoodAnalyserFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UC5MoodAnalyser
+{
+    class MoodAnalyserFactory
+    {
+        private string message;
+        public MoodAnalyserFactory(string message)
+        {
+            this.message = message;
+        }
+
+       
+
+        // UC-5
+        public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName, string message)
+        {
+            Type type = typeof(MoodAnalyser);
+            if (type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] { typeof(string) });
+                    object instance = ctor.Invoke(new object[] { message });
+                    return instance;
+                }
+                else
+                {
+                    throw new CException(CException.ExceptionType.no_such_class, "Class not found");
+                    throw new CException(CException.ExceptionType.no_such_method, "Constructor is not found");
+                }
+            }
+            else
+            {
+                throw new CException(CException.ExceptionType.no_such_class, "Class not found");
+
+            }
+        }
+    }
+}
