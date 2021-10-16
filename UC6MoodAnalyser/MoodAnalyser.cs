0538b3509baa3bc4f2234c9f22ca43d4d2370581
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-16 17:30:09 +0530

    Hemanth: Analyse Mood using reflection

---
+++ UC6MoodAnalyser/MoodAnalyser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC6MoodAnalyser
+{
+    class MoodAnalyser
+    {
+        string message;
+
+        public MoodAnalyser()
+        {
+        }
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new CException(CException.ExceptionType.empty_message, "Mood should not be EMPTY");
+                    throw new Exception();
+                }
+                if (this.message.Contains("sad"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+            }
+            catch (NullReferenceException e)
+            {
+                return e.Message;
+
+
+            }
+        }
+    }
+}
---
+++ UC6MoodAnalyser/MoodAnalyserFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UC6MoodAnalyser
+{
+    class MoodAnalyserFactory
+    {
+        private string message;
+        public MoodAnalyserFactory(string message)
+        {
+            this.message = message;
+        }
+
+
+
+        // UC-5
+        public static string InvokeAnalyserMood(string message, string methodName)
+        {
+            try
+            {
+                Type type = Type.GetType("Day20_MoodAnalyser_Test_Assignment.MoodAnalyser");
+                object moodAnalyserObject = MoodAnalyserFactory.CreateMoodAnalyseParameterizedConstructor("Day20_MoodAnalyser_Test_Assignment.MoodAnalyser", "MoodAnalyser", message);
+                MethodInfo analyseMoodInfo = type.GetMethod(methodName);
+                object mood = analyseMoodInfo.Invoke(moodAnalyserObject, null);
+                return mood.ToString();
+            }
+            catch (NullReferenceException)
+            {
+                throw new CException(CException.ExceptionType.no_such_method, "Method is Not Found");
+            }
+        }
+    }
+}
